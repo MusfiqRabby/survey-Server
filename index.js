eb76f159ebb3946d78e98f69800c758a41146738
musfiqrabby <musfiqrabby69@gmail.com>
2024-07-15 10:48:48 +0600

    payment

--- index.js
+++ index.js
@@ -230,16 +230,22 @@ async function run() {
       res.send(result)
     })
 
-    
+    app.get('/payments', async(req, res)=>{
+      const cursor = paymentsCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
-
+    app.post('/payments', async(req, res)=>{
+      const payments = req.body
+      const resultPayment = await paymentsCollection.insertOne(payments)
+      res.send(resultPayment)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
